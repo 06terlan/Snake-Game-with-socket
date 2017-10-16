be93090506608d6d3eafeff1f156baee158ec1d7
06terlan <06terlan91@gmail.com>
2017-10-16 22:44:44 +0500

    Server

--- Socket/BaseServer.php
+++ Socket/BaseServer.php
@@ -57,7 +57,7 @@ abstract class BaseServer
 		$this->address = $address;
 		$this->port = $port;
 		$this->cprint = $cprint;
-		$this->console("Server starting...");
+		$this->consoleWrite("Server starting...");
 
 		/**
 		 * socket creation
@@ -66,28 +66,28 @@ abstract class BaseServer
 		 * @values SOL_TCP - reliable, connection based, stream oriented, full duplex protocol
 		 */
 		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) 
-								and $this->console("socket_create() done: ")
-								or $this->console("socket_create() failed: ".socket_strerror(socket_last_error()),true);
+								and $this->consoleWrite("socket_create() done: ")
+								or $this->consoleWrite("socket_create() failed: ".socket_strerror(socket_last_error()),true);
 		socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1)
-								and $this->console("socket_set_option() done: ")
-								or $this->console("socket_set_option() failed: ".socket_strerror(socket_last_error()),true);
+								and $this->consoleWrite("socket_set_option() done: ")
+								or $this->consoleWrite("socket_set_option() failed: ".socket_strerror(socket_last_error()),true);
 		socket_bind($socket, $this->address, $this->port)
-								and $this->console("socket_bind() done: ")
-								or $this->console("socket_bind() failed: ".socket_strerror(socket_last_error()),true);
+								and $this->consoleWrite("socket_bind() done: ")
+								or $this->consoleWrite("socket_bind() failed: ".socket_strerror(socket_last_error()),true);
 		socket_listen($socket, 20)
-								and $this->console("socket_listen() done: ")
-								or $this->console("socket_listen() failed: ".socket_strerror(socket_last_error()),true);
+								and $this->consoleWrite("socket_listen() done: ")
+								or $this->consoleWrite("socket_listen() failed: ".socket_strerror(socket_last_error()),true);
 
 		$this->master = $socket;
 		$this->sockets = array($socket);
-		$this->console("Server started on {$this->address}:{$this->port}");
+		$this->consoleWrite("Server started on {$this->address}:{$this->port}");
 	}
 
 	/******************** Some usefull events *************************/
-	abstract protected function process($user,$message); // Called immediately when the data is recieved. 
-  	abstract protected function connected($user);        // Called after the handshake response is sent to the client.
-  	abstract protected function closed($user);           // Called after the connection is closed.
-  	abstract protected function connecting($user);       // Called after the client instance had been created
+	abstract protected function process($client,$message); // Called immediately when the data is recieved. 
+  	abstract protected function connected($client);        // Called after the handshake response is sent to the client.
+  	abstract protected function closed($client);           // Called after the connection is closed.
+  	abstract protected function connecting($client);       // Called after the client instance had been created
 	/******************** Some usefull events *************************/
 
 	/**
@@ -96,7 +96,7 @@ abstract class BaseServer
 	public function run() 
 	{
 		$i = 0;
-		$this->console("Start running...");
+		$this->consoleWrite("Start running...");
 		while(true)
 		{
 			$changed_sockets = $this->sockets;
@@ -108,22 +108,22 @@ abstract class BaseServer
 				{
 					($acceptedSocket = socket_accept($this->master))
 						and $this->connect($acceptedSocket)
-						or $this->console("Socket error: ".socket_strerror(socket_last_error($acceptedSocket)));
+						or $this->consoleWrite("Socket error: ".socket_strerror(socket_last_error($acceptedSocket)));
 				}
 				else
 				{
-					$this->console("Finding the socket that associated to the client...");
+					$this->consoleWrite("Finding the socket that associated to the client...");
 					$client = $this->clients[$s_id];// $this->getClientBySocket($socket);
 					if($client)
 					{
-						$this->console("Receiving data from the client");
+						$this->consoleWrite("Receiving data from the client");
 
 						$data = null;
 
 						$bytes = socket_recv($socket, $r_data, 2048, 0);	$data .= $r_data;
 						if(!$client->getHandshake())
 						{
-							$this->console("Doing the handshake");
+							$this->consoleWrite("Doing the handshake");
 							if(!$this->handshake($client, $data))
 							{
 								$this->disconnect($client);
@@ -141,11 +141,13 @@ abstract class BaseServer
 					}
 				}
 			}
-			if( file_get_contents(__DIR__ . "/action") == "exit" )
+
+			if( file_get_contents( __DIR__ . "/action") == "exit" )
 			{
-				$this->console("Action shutDownServer");
-				$this->shutDownServer(); exit();
+				$this->shutDownServer();
+				$this->consoleWrite("Action shutDownServer",true);
 			}
+
 		}
 	}
 
@@ -157,7 +159,7 @@ abstract class BaseServer
 	private function action($client, $data)
 	{
 		$data = $this->unmask($data);
-		$this->console("Performing data: ".$data);
+		$this->consoleWrite("Performing data: ".$data);
 		$this->process($client,$data);
 	}
 
@@ -195,16 +197,16 @@ abstract class BaseServer
 	 */
 	private function disconnect($client) 
 	{
-		$this->console("Disconnecting client #{$client->getId()}");
+		$this->consoleWrite("Disconnecting client #{$client->getId()}");
 	
 		unset($this->clients[$client->getId()]);
 		unset($this->sockets[$client->getId()]);
 
 		socket_shutdown($client->getSocket(), 2);
 		socket_close($client->getSocket());
-		$this->console("Socket closed");
+		$this->consoleWrite("Socket closed");
 
-		$this->console("Client #{$client->getId()} disconnected");
+		$this->consoleWrite("Client #{$client->getId()} disconnected");
 		$this->closed($client);
 	}
 
@@ -215,22 +217,22 @@ abstract class BaseServer
 	 */
 	private function handshake($client, $headers)
 	{
-		$this->console("Getting client WebSocket version...");
+		$this->consoleWrite("Getting client WebSocket version...");
 		if(preg_match("/Sec-WebSocket-Version: (.*)\r\n/", $headers, $match))
 		{
 			$version = $match[1];
 		}
 		else
 		{
-			$this->console("The client doesn't support WebSocket");
+			$this->consoleWrite("The client doesn't support WebSocket");
 			return false;
 		}
 
-		$this->console("Client WebSocket version is {$version}, (required: 13)");
+		$this->consoleWrite("Client WebSocket version is {$version}, (required: 13)");
 		if($version == 13)
 		{
 			// Extract header variables
-			$this->console("Getting headers...");
+			$this->consoleWrite("Getting headers...");
 			if(preg_match("/GET (.*) HTTP/", $headers, $match))
 				$root = $match[1];
 			if(preg_match("/Host: (.*)\r\n/", $headers, $match))
@@ -240,13 +242,13 @@ abstract class BaseServer
 			if(preg_match("/Sec-WebSocket-Key: (.*)\r\n/", $headers, $match))
 				$key = $match[1];
 
-			$this->console("Client headers are:");
-			$this->console("\t- Root: ".$root);
-			$this->console("\t- Host: ".$host);
-			$this->console("\t- Origin: ".$origin);
-			$this->console("\t- Sec-WebSocket-Key: ".$key);
+			$this->consoleWrite("Client headers are:");
+			$this->consoleWrite("\t- Root: ".$root);
+			$this->consoleWrite("\t- Host: ".$host);
+			$this->consoleWrite("\t- Origin: ".$origin);
+			$this->consoleWrite("\t- Sec-WebSocket-Key: ".$key);
 
-			$this->console("Generating Sec-WebSocket-Accept key...");
+			$this->consoleWrite("Generating Sec-WebSocket-Accept key...");
 			$acceptKey = $key.'258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
 			$acceptKey = base64_encode(sha1($acceptKey, true));
 
@@ -256,16 +258,16 @@ abstract class BaseServer
 					   "Sec-WebSocket-Accept: $acceptKey".
 					   "\r\n\r\n";
 
-			$this->console("Sending this response to the client #{$client->getId()}:\r\n".$upgrade);
+			$this->consoleWrite("Sending this response to the client #{$client->getId()}:\r\n".$upgrade);
 			socket_write($client->getSocket(), $upgrade);
 			$client->setHandshake(true);
-			$this->console("Handshake is successfully done!");
+			$this->consoleWrite("Handshake is successfully done!");
 			$this->connected($client);
 			return true;
 		}
 		else
 		{
-			$this->console("WebSocket version 13 required (the client supports version {$version})");
+			$this->consoleWrite("WebSocket version 13 required (the client supports version {$version})");
 			return false;
 		}
 	}
@@ -280,7 +282,7 @@ abstract class BaseServer
 		foreach($this->clients as $client)
 			if($client->getSocket() == $socket)
 			{
-				$this->console("Client found");
+				$this->consoleWrite("Client found");
 				return $client;
 			}
 		return false;
@@ -292,11 +294,11 @@ abstract class BaseServer
 	 */
 	private function connect($socket)
 	{
-		$this->console("Creating client...");
+		$this->consoleWrite("Creating client...");
 		$client = new \WebSocket\Client(uniqid(), $socket);
 		$this->clients[$client->getId()] = $client;
 		$this->sockets[$client->getId()] = $socket;
-		$this->console("Client #{$client->getId()} is successfully created!");
+		$this->consoleWrite("Client #{$client->getId()} is successfully created!");
 		$this->connecting($client);
 
 		return true;
@@ -308,7 +310,7 @@ abstract class BaseServer
 	public function shutDownServer()
 	{
 		socket_close($this->master);
-		$this->console("Server shutted down",false,"red");
+		$this->consoleWrite("Server shutted down",false,"red");
 	}
 
 	/**
@@ -316,7 +318,7 @@ abstract class BaseServer
 	 * @param $text the text to display
 	 * @param $exit if true, the process will exit
 	 */
-	private function console($text , $exit = false) 
+	private function consoleWrite($text , $exit = false) 
 	{
 		$text = date('(Y-m-d H:i:s) - ').$text;
 		if($exit) {
@@ -328,5 +330,109 @@ abstract class BaseServer
 		}
 
 		return true;
+	}
+
+	/**
+	 * Send a text to client
+	 * @param $client
+	 * @param $text
+	 */
+	protected function send($client, $text)
+	{
+		$this->consoleWrite("Send '".$text."' to client #{$client->getId()}");
+		$text = $this->encode($text);
+		if(socket_write($client->getSocket(), $text, strlen($text)) === false)
+		{
+			$this->consoleWrite("Unable to write to client #{$client->getId()}'s socket");
+			$this->disconnect($client);
+		}
+	}
+
+	/**
+	 * Encode a text for sending to clients via ws://
+	 * @param $text
+	 * @param $messageType
+	 */
+	function encode($message, $messageType='text')
+	{
+		switch ($messageType)
+		{
+			case 'continuous':
+				$b1 = 0;
+				break;
+			case 'text':
+				$b1 = 1;
+				break;
+			case 'binary':
+				$b1 = 2;
+				break;
+			case 'close':
+				$b1 = 8;
+				break;
+			case 'ping':
+				$b1 = 9;
+				break;
+			case 'pong':
+				$b1 = 10;
+				break;
+		}
+
+		$b1 += 128;
+
+
+		$length = strlen($message);
+		$lengthField = "";
+
+		if($length < 126)
+		{
+			$b2 = $length;
+		} 
+		elseif($length <= 65536)
+		{
+			$b2 = 126;
+			$hexLength = dechex($length);
+			//$this->stdout("Hex Length: $hexLength");
+			if(strlen($hexLength)%2 == 1)
+			{
+				$hexLength = '0' . $hexLength;
+			}
+
+			$n = strlen($hexLength) - 2;
+
+			for($i = $n; $i >= 0; $i=$i-2)
+			{
+				$lengthField = chr(hexdec(substr($hexLength, $i, 2))) . $lengthField;
+			}
+
+			while(strlen($lengthField) < 2)
+			{
+				$lengthField = chr(0) . $lengthField;
+			}
+
+		} else
+		{
+
+			$b2 = 127;
+			$hexLength = dechex($length);
+
+			if(strlen($hexLength) % 2 == 1)
+			{
+				$hexLength = '0' . $hexLength;
+			}
+
+			$n = strlen($hexLength) - 2;
+
+			for($i = $n; $i >= 0; $i = $i - 2)
+			{
+				$lengthField = chr(hexdec(substr($hexLength, $i, 2))) . $lengthField;
+			}
+
+			while(strlen($lengthField) < 8)
+			{
+				$lengthField = chr(0) . $lengthField;
+			}
+		}
+
+		return chr($b1) . chr($b2) . $lengthField . $message;
 	}
 }
--- Socket/Server.php
+++ Socket/Server.php
@@ -10,24 +10,25 @@ namespace WebSocket;
 
 class Server extends BaseServer
 {
-  protected function process ($user, $message)
+  protected function process ($client, $message)
   {
      // Do nothing: This runs after if any data comes from client
   }
 
-  protected function connecting($user)
+  protected function connecting($client)
   {
     //  after the instance of the User is created
   }
   
-  protected function connected ($user)
+  protected function connected ($client)
   {
     // Do nothing: This is just an echo server, there's no need to track the user.
     // However, if we did care about the users, we would probably have a cookie to
     // parse at this step, would be looking them up in permanent storage, etc.
+    $this->send($client,"Hi new client");
   }
   
-  protected function closed ($user)
+  protected function closed ($client)
   {
     // Do nothing: This is where cleanup would go, in case the user had any sort of
     // open files or other objects associated with them.  This runs after the socket 
